e7a7156e6e912cb026f519800515866e5eec255c
Malte Hecht <malte@siggi.cloud>
2022-09-20 10:18:28 +0200

    try VitePWA

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,34 @@
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
+import { VitePWA } from "vite-plugin-pwa";
 import tsconfigPaths from "vite-tsconfig-paths";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), tsconfigPaths()],
+  plugins: [
+    react(),
+    tsconfigPaths(),
+    VitePWA({
+      registerType: "autoUpdate",
+      includeAssets: ["icon.png", "apple-touch-icon.png", "masked-icon.svg"],
+      manifest: {
+        name: "PomoDo",
+        short_name: "PomoDo",
+        description: "ToDo + Pomodoro + Marketing/Coding Weeks",
+        theme_color: "#000000",
+        icons: [
+          {
+            src: "icon-192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "icon-512.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+        ],
+      },
+    }),
+  ],
 });
